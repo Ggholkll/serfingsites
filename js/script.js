02393a5b50795a20aaf66a14e100cff0aa0cd9f4
Ggholkll <112123876+Ggholkll@users.noreply.github.com>
2024-04-15 13:42:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ elementHeaderMenuBurger.addEventListener('click' , function () {
 		document.body.style.overflow = 'auto'
 	}
 })
+
+$(document).ready(function(){
+	$('.menu li a').click(function () { // 1
+		elementClick = $(this).attr('href');
+		destination = $(elementClick).offset().top;
+		$('body,html').animate({scrollTop: destination }, 1400);
+	});
+});
